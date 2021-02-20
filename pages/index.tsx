b627bed2ab9c95a70a05fbbb54f1a681fd99cafd
grims-dev <ciarangrimshawdev@gmail.com>
2021-02-20 16:15:55 +0000

    Initial homepage design

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,15 @@ export default function Home() {
         <link rel="preload" href="/fonts/Oxygen/Oxygen-Light.ttf" as="font" crossOrigin="" />
         <link rel="preload" href="/fonts/Oxygen/Oxygen-Bold.ttf" as="font" crossOrigin="" />
       </Head>
-      <h1>Spotify Stats</h1>
+      <div className="md:flex">
+        <div className="p-4 md:w-3/5 md:mt-52">
+          <h1 className="text-9xl text-green-600">Spotify Stats</h1>
+        </div>
+        <div className="p-4 md:w-2/5 md:mt-60 text-2xl leading-normal">
+          <p className="mb-6">Log in with Spotify to view your listening habits in charts and graphs.</p>
+          <button className="px-4 py-2 bg-green-600 rounded shadow-md text-xl">Login</button>
+        </div>
+      </div>
     </div>
   );
 }
