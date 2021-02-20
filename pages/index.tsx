4c66d9fd717ef7d7d6b32dbe33e18458a7685ce8
grims-dev <ciarangrimshawdev@gmail.com>
2021-02-20 17:36:02 +0000

    Add some dummy logged in functionality, update content, add new stats page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import Head from "next/head";
+import Link from 'next/link'
 import styles from "../styles/Home.module.css";
+
+let isLoggedIn = true;
+const user = {
+  username: "ciaran_grimshaw"
+}
 
 export default function Home() {
   return (
@@ -7,7 +13,7 @@ export default function Home() {
       <Head>
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title>Spotify Stats</title>
+        <title>Home | Spotify Stats</title>
         <meta name="description" content="View your Spotify listening habits in charts and graphs"></meta>
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
         <link rel="preload" href="/fonts/Oxygen/Oxygen-Light.ttf" as="font" crossOrigin="" />
@@ -18,8 +24,12 @@ export default function Home() {
           <h1 className="text-9xl text-green-600">Spotify Stats</h1>
         </div>
         <div className="p-4 md:w-2/5 md:mt-60 text-2xl leading-normal">
-          <p className="mb-6">Log in with Spotify to view your listening habits in charts and graphs.</p>
-          <button className="px-4 py-2 bg-green-600 rounded shadow-md text-xl">Login</button>
+          {isLoggedIn ? (
+            <p className="mb-6">You are logged in as <strong>{user.username}</strong>! View the <Link href="/stats">stats page</Link></p>
+          ) : (
+            <p className="mb-6">Log in with Spotify to view your listening habits in charts and graphs.</p>
+          )}
+          <button className="px-4 py-2 bg-green-600 rounded shadow-md text-xl">{isLoggedIn ? "Log out" : "Log in"}</button>
         </div>
       </div>
     </div>
---
+++ pages/stats.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+
+export default function Stats() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Stats | Spotify Stats</title>
+        <meta name="description" content="View your Spotify listening habits in charts and graphs"></meta>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+        <link rel="preload" href="/fonts/Oxygen/Oxygen-Light.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/Oxygen/Oxygen-Bold.ttf" as="font" crossOrigin="" />
+      </Head>
+      <div>
+        <p>Stats!</p>
+      </div>
+    </div>
+  );
+}
