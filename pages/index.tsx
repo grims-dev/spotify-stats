9e3019308b749922df0e7ff85d5f54bd786818f3
grims-dev <ciarangrimshawdev@gmail.com>
2021-02-20 13:54:37 +0000

    Add initial head tags to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,13 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Spotify Stats</title>
+        <meta name="description" content="View your Spotify listening habits in charts and graphs"></meta>
+
+      </Head>
       <h1>Spotify Stats</h1>
     </div>
   );
