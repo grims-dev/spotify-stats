87f5fbc805a06d14847b58a45901e84482e3fda5
grims-dev <ciarangrimshawdev@gmail.com>
2021-02-20 13:11:16 +0000

    Import Tailwind in app and reformat files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Spotify Stats</h1>
     </div>
-  )
+  );
 }
