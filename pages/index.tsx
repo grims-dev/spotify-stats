0d049f5273650d569391b6b14619255fa3a8dc78
grims-dev <ciarangrimshawdev@gmail.com>
2021-02-23 16:54:17 +0000

    Update mobile styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,16 +21,16 @@ export default function Home() {
         <link rel="preload" href="/fonts/Oxygen/Oxygen-Bold.ttf" as="font" crossOrigin="" />
       </Head>
       <div className="md:flex">
-        <div className="p-4 md:w-3/5 md:mt-52">
-          <h1 className="text-9xl text-green-500">Spotify Stats</h1>
+        <div className="p-4 md:w-3/5 mt-8 md:mt-52">
+          <h1 className="text-7xl md:text-9xl text-green-600 text-center md:text-left">Spotify Stats</h1>
         </div>
-        <div className="p-4 md:w-2/5 md:mt-60 text-2xl leading-normal">
+        <div className="p-4 md:w-2/5 md:mt-60 text-xl md:text-2xl leading-normal text-center md:text-left">
           {isLoggedIn ? (
             <p className="mb-6">You are logged in as <strong>{user.username}</strong>! View the <Link href="/stats">stats page</Link></p>
           ) : (
             <p className="mb-6">Log in with Spotify to view your listening habits in charts and graphs.</p>
           )}
-          <button className="px-4 py-2 bg-green-500 rounded shadow-md text-xl" onClick={redirectToAuth}>{isLoggedIn ? "Log out" : "Log in"}</button>
+          <button className="px-4 py-2 bg-green-600 rounded shadow-md text-xl" onClick={redirectToAuth}>{isLoggedIn ? "Log out" : "Log in"}</button>
         </div>
       </div>
     </div>
