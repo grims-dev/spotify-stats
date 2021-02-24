eb12f5a90fbda6f11045a4a0e10e887095e87e89
grims-dev <ciarangrimshawdev@gmail.com>
2021-02-24 19:50:47 +0000

    Refactor redirectToAuth function for readability

--- lib/userAuth.ts
+++ lib/userAuth.ts
@@ -1,5 +1,9 @@
 export function redirectToAuth() {
-  const scopes = 'user-read-recently-played user-top-read playlist-read-private playlist-read-collaborative user-library-read';
-  const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI)}&scope=${encodeURIComponent(scopes)}`
+  const scopes = 'user-read-recently-played user-top-read playlist-read-private playlist-read-collaborative';
+  let authUrl = 'https://accounts.spotify.com/authorize';
+  authUrl += '?response_type=code';
+  authUrl += '&client_id=' + process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+  authUrl += '&redirect_uri=' + encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI);
+  authUrl += '&scope=' + encodeURIComponent(scopes);
   window.location.assign(authUrl);
 }
