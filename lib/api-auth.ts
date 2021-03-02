b836cea4ac43fb488d514a74487c82c5fa647ef4
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-02 17:45:29 +0000

    Add basic types to string

--- lib/api-auth.ts
+++ lib/api-auth.ts
@@ -1,6 +1,6 @@
 export function redirectToUserAuth() {
-  const scopes = 'user-read-recently-played user-top-read playlist-read-private playlist-read-collaborative';
-  let authUrl = 'https://accounts.spotify.com/authorize';
+  const scopes: string = 'user-read-recently-played user-top-read playlist-read-private playlist-read-collaborative';
+  let authUrl: string = 'https://accounts.spotify.com/authorize';
   authUrl += '?response_type=code';
   authUrl += '&client_id=' + process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
   authUrl += '&redirect_uri=' + encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI);
