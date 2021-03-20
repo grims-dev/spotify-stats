37e542e3126049a486105c6765db2634672c41c5
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-20 18:36:11 +0000

    Add auth page that grabs an auth code from the Spotify redirect and requests an access token

---
+++ pages/auth.tsx
@@ -0,0 +1,30 @@
+import Cookies from 'cookies';
+import { requestAccessToken } from '../lib/api-auth';
+
+export default function Auth() {
+  return null;
+}
+
+export async function getServerSideProps({ req, res, query }) {
+  const homeRedirect = {
+      redirect: {
+      permanent: false,
+      destination: "/"
+    }
+  }
+  const cookies = new Cookies(req, res);
+  const existingAccessResponse = cookies.get('accessResponse') ? JSON.parse(cookies.get('accessResponse')) : false;
+  if (existingAccessResponse && new Date().getTime() < existingAccessResponse.expires_in_unix_timestamp) return homeRedirect;
+
+  let code = query.code || req.cookies.code;
+  if (!code) return homeRedirect;
+  cookies.set('code', code);
+
+  const accessResponse = await requestAccessToken(code);
+  if (accessResponse.error) return homeRedirect;
+  // append absolute timestamp to object
+  accessResponse.expires_in_unix_timestamp = new Date().getTime() + (accessResponse.expires_in * 1000);
+  cookies.set('accessResponse', JSON.stringify(accessResponse));
+
+  return homeRedirect;
+}
