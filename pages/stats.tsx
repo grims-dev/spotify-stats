eee0512e6a09b7907754c33b1f53aaa18bdb5f77
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-20 18:43:15 +0000

    Replace " with ' for consistency

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 import { createGetRequestOptions } from '../lib/fetch-helpers';
 
 export default function Stats({ test }) {
