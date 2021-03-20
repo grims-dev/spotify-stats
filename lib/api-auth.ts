a48c0be780ff53f68d2e14daf6805c327fc32512
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-20 18:28:07 +0000

    Add requestAccessToken API function that retrieves an access token using an auth code or refresh token

--- lib/api-auth.ts
+++ lib/api-auth.ts
@@ -7,3 +7,20 @@ export function redirectToUserAuth() {
   authUrl += '&scope=' + encodeURIComponent(scopes);
   window.location.assign(authUrl);
 }
+
+// call this with the auth code on first go around, otherwise call with refresh token
+export async function requestAccessToken(authCodeOrRefreshToken: string) {
+  const base64ClientIdAndSecret = Buffer.from(`${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
+  const postRequestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Basic ${base64ClientIdAndSecret}`,
+    },
+    body: 'grant_type=authorization_code&code=' + authCodeOrRefreshToken + '&redirect_uri=' + process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI
+  }
+  return fetch('https://accounts.spotify.com/api/token', postRequestOptions)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+    .then((json) => { return json });
+}
