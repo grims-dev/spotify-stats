ddf3cd15165dfd52d181a1ee9bcfd08c7bc2d547
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-20 18:38:50 +0000

    Connect up to auth page by checking cookies Remove unused query param functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useRouter } from 'next/router'
+import Cookies from 'cookies';
 import { redirectToUserAuth } from '../lib/api-auth';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  const router = useRouter();
-  const spotifyToken = router.query?.code;
-
+export default function Home({ isAuthed = false }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -24,14 +21,36 @@ export default function Home() {
           <h1 className="text-7xl md:text-9xl text-green-600 text-center md:text-left">Spotify Stats</h1>
         </div>
         <div className="p-4 md:w-2/5 md:mt-60 text-xl md:text-2xl leading-normal text-center md:text-left">
-          {spotifyToken ? (
+          {isAuthed ? (
             <p className="mb-6">You are logged in with Spotify! View the <Link href="/stats">stats page</Link> &raquo;</p>
           ) : (
-            <p className="mb-6">Log in with Spotify to view your listening habits in charts and graphs.</p>
+            <>
+              <p className="mb-6">Log in with Spotify to view your listening habits in charts and graphs.</p>
+              <button className="px-4 py-2 bg-green-600 rounded shadow-md text-xl" onClick={redirectToUserAuth}>Log in</button>
+            </>
           )}
-          <button className="px-4 py-2 bg-green-600 rounded shadow-md text-xl" onClick={redirectToUserAuth}>{spotifyToken ? "Log out" : "Log in"}</button>
         </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps({ req, res }) {
+  const cookies = new Cookies(req, res);
+  const existingAccessResponse = cookies.get('accessResponse') ? JSON.parse(cookies.get('accessResponse')) : false;
+  if (!existingAccessResponse) {
+    return { props: { isAuthed: false } }
+  }
+
+  // redirect to auth page with refresh token if access has expired
+  if (new Date().getTime() >= existingAccessResponse.expires_in_unix_timestamp) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/auth?code=${existingAccessResponse.refresh_token}`
+      }
+    }
+  }
+
+  return { props: { isAuthed: true } }
 }
