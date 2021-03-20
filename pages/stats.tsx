0d2fc54c9ccf2b709f8842d1218013529dac937c
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-20 19:01:10 +0000

    Add auth functionality to stats page with a test call using getUserOwnedFollowedPlaylists

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
+import Cookies from 'cookies';
 import styles from '../styles/Home.module.css';
 import { createGetRequestOptions } from '../lib/fetch-helpers';
+import { getUserOwnedFollowedPlaylists } from '../lib/api-methods';
 
-export default function Stats({ test }) {
-  console.log("hi!", test);
+export default function Stats({ isAuthed = false }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,16 +17,32 @@ export default function Stats({ test }) {
         <link rel="preload" href="/fonts/Oxygen/Oxygen-Bold.ttf" as="font" crossOrigin="" />
       </Head>
       <div>
-        <p>Stats page!</p>
+        <p>Stats page! You are {isAuthed ? 'authed' : 'not authed'}.</p>
       </div>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const getRequestOptions = createGetRequestOptions(process.env.SPOTIFY_CLIENT_SECRET);
+export async function getServerSideProps({ req, res }) {
+  const cookies = new Cookies(req, res);
+  const existingAccessResponse = cookies.get('accessResponse') ? JSON.parse(cookies.get('accessResponse')) : false;
+  if (!existingAccessResponse) {
+    return { props: { isAuthed: false } }
+  }
 
+  // redirect to auth page with refresh token if access has expired
+  if (new Date().getTime() >= existingAccessResponse.expires_in_unix_timestamp) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/auth?code=${existingAccessResponse.refresh_token}`
+      }
+    }
+  }
+
+  const requestOptions = createGetRequestOptions(existingAccessResponse.access_token);
+  console.log(await getUserOwnedFollowedPlaylists(requestOptions));
   return {
-    props: { test: getRequestOptions },
+    props: { isAuthed: true },
   }
 }
