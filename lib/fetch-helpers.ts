075198676b552de7197c5d6ef19e01d2da3a9265
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-21 12:46:26 +0000

    Add getRequest helper function

--- lib/fetch-helpers.ts
+++ lib/fetch-helpers.ts
@@ -6,3 +6,10 @@ export function createGetRequestOptions(accessToken: string) {
     },
   }
 }
+
+export async function getRequest(endpointURL: string, accessToken: string) {
+  return fetch(endpointURL, createGetRequestOptions(accessToken))
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+    .then((json) => { return json })
+}
