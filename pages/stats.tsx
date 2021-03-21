35d7d4262fd0aec22c80628efe6b0049ff36c044
grims-dev <ciarangrimshawdev@gmail.com>
2021-03-21 18:28:44 +0000

    Initial generateTopTable function

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,17 +1,54 @@
 import Cookies from 'cookies';
 import HeadTags from '../components/headTags';
 import styles from '../styles/Home.module.css';
-import { getUserOwnedFollowedPlaylists } from '../lib/api-methods';
+import { redirectToUserAuth } from '../lib/api-auth';
+import { getUserOwnedFollowedPlaylists, getUsersTopArtistsOrTracks } from '../lib/api-methods';
 
 export default function Stats({ isAuthed = false, data }) {
+  function generateTopTable() {
+    const parsedItems = JSON.parse(data).items;
+    console.log(parsedItems);
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>No.</th>
+            <th>Song</th>
+            <th>Artist(s)</th>
+          </tr>
+          {parsedItems.map((item, i) => {
+            return (
+              <tr>
+                <td>{i + 1}.</td>
+                <td>{item.name}</td>
+                <td>{item.artists.map(artist => artist.name).join(', ')}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    );
+  }
+  if (!isAuthed) { 
+    return (
+      <div className={styles.container}>
+        <HeadTags title="Stats" />
+        <div className="p-4 md:mt-48 text-xl md:text-2xl leading-normal text-center">
+          <p className="mb-6">Sorry, your stats are only available if you log in with Spotify.</p>
+          <button className="px-4 py-2 bg-green-600 rounded shadow-md text-xl" onClick={redirectToUserAuth}>Log in</button>
+        </div>
+      </div>
+    );
+  }
+
+  
   return (
     <div className={styles.container}>
       <HeadTags title="Stats" />
-      <div>
+      <div className="p-4 md:mt-12 text-xl md:text-2xl leading-normal">
         <p>Stats page! You are {isAuthed ? 'authed' : 'not authed'}.</p>
-        <code>
-          {data}
-        </code>
+        <p>Your top artists:</p>
+        {generateTopTable()}
       </div>
     </div>
   );
@@ -34,6 +71,6 @@ export async function getServerSideProps({ req, res }) {
     }
   }
 
-  const playlistData = await getUserOwnedFollowedPlaylists(existingAccessResponse.access_token);
+  const playlistData = await getUsersTopArtistsOrTracks(existingAccessResponse.access_token, 'tracks', 'long_term');
   return { props: { isAuthed: true, data: JSON.stringify(playlistData) } }
 }
