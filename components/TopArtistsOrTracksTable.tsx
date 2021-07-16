ca0b775a36bfa2e6218aea21f1f28ec98f70b9df
grims-dev <ciarangrimshawdev@gmail.com>
2021-07-16 23:11:39 +0100

    Refactor TopArtistsOrTracksTable component

--- components/TopArtistsOrTracksTable.tsx
+++ components/TopArtistsOrTracksTable.tsx
@@ -3,40 +3,64 @@ import useTopArtistsOrTracks from "../hooks/useTopArtistsOrTracks";
 
 export default function TopArtistsOrTracksTable({ accessToken , endpointOptions }) {
   const { data, isLoading, isError } = useTopArtistsOrTracks(accessToken, endpointOptions);
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>An error occurred.</div>;
- 
-  return (
-    <table className='w-full text-base'>
+  if (isLoading) return <>Loading...</>;
+  if (isError) return <>An error occurred when connecting to Spotify. Please try reloading the page.</>;
+  if (data?.items) return (
+    <table className="w-full text-base">
       <tbody>
-        <tr className='border-b border-gray-500 text-left'>
-          <th className="p-1.5"></th>
-          <th className='p-1.5'>No.</th>
-          <th className='p-1.5'>Title</th>
-          <th className='p-1.5'>Artist(s)</th>
-        </tr>
-        {data && console.log(data)}
-        {data?.items && endpointOptions.topType === 'tracks' && data.items?.map((track, i) => {
-          const alternateBackground = i % 2 === 0 ? 'bg-white bg-opacity-5' : '';
-          return (
-            <tr key={track.id} className={alternateBackground}>
-              <td className="p-1.5 w-12"><img src={track.album.images[track.album.images.length - 1].url} alt={`${track.album.name} art`}/></td>
-              <td className='p-1.5'>{i + 1}.</td>
-              <td className='p-1.5'>{track.name}</td>
-              <td className='p-1.5'>{track.artists.map(artist => artist.name).join(', ')}</td>
+        {endpointOptions.topType === 'tracks' && (
+          <>
+            <tr className="border-b border-gray-500 text-left sticky top-0">
+              <th className="p-1.5">No.</th>
+              <th className="p-1.5">Icon</th>
+              <th className="p-1.5">Title</th>
+              <th className="p-1.5">Artist(s)</th>
             </tr>
-          )
-        })}
-        {data?.items && endpointOptions.topType === 'artists' && data.items?.map((artist, i) => {
-          const alternateBackground = i % 2 === 0 ? 'bg-white bg-opacity-5' : '';
-          return (
-            <tr key={artist.id} className={alternateBackground}>
-              <td className='p-1.5'>{i + 1}.</td>
-              <td className='p-1.5'>{artist.name}</td>
+            {data.items?.map((track, i) => {
+              const alternateBackground = i % 2 === 0 ? 'bg-white bg-opacity-5' : '';
+              return (
+                <tr key={track.id} className={alternateBackground}>
+                  <td className="p-1.5 text-center">{i + 1}.</td>
+                  <td className="p-1.5">
+                    <div className="w-12 h-12">
+                      <img className="w-full max-h-full object-cover" src={track.album.images[track.album.images.length - 1].url} alt={`${track.album.name} art`}/>
+                    </div>
+                  </td>
+                  <td className="p-1.5">{track.name}</td>
+                  <td className="p-1.5">{track.artists.map(artist => artist.name).join(', ')}</td>
+                </tr>
+              )
+            })}
+          </>
+        )}
+        {endpointOptions.topType === 'artists' && (
+          <>
+            <tr className="border-b border-gray-500 text-left sticky top-0">
+              <th className="p-1.5">No.</th>
+              <th className="p-1.5">Icon</th>
+              <th className="p-1.5">Artist</th>
+              <th className="p-1.5">Genre(s)</th>
             </tr>
-          )
-        })}
+            {data.items?.map((artist, i) => {
+              const alternateBackground = i % 2 === 0 ? 'bg-white bg-opacity-5' : '';
+              return (
+                <tr key={artist.id} className={alternateBackground}>
+                  <td className="p-1.5 text-center">{i + 1}.</td>
+                  <td className="p-1.5">
+                    <div className="w-16 h-16">
+                      <img className="w-full max-h-full object-cover" src={artist.images[artist.images.length - 1].url} alt={`${artist.name} icon`}/>
+                    </div>
+                    </td>
+                  <td className="p-1.5">{artist.name}</td>
+                  <td className="p-1.5">{artist.genres.join(', ') || '(no recorded genres)'}</td>
+                </tr>
+              )
+            })}
+          </>
+        )}
       </tbody>
     </table>
   )
+
+  return <>An error has occurred. Please try reloading the page.</>;
 }
