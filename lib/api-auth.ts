2f51257cebe1057d191526da92febbf67102417e
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-15 15:41:21 +0100

    Standardise string building with template literals

--- lib/api-auth.ts
+++ lib/api-auth.ts
@@ -1,10 +1,10 @@
 export function redirectToUserAuth() {
   const scopes: string = 'user-read-recently-played user-top-read playlist-read-private playlist-read-collaborative';
-  let authUrl: string = 'https://accounts.spotify.com/authorize';
-  authUrl += '?response_type=code';
-  authUrl += '&client_id=' + process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
-  authUrl += '&redirect_uri=' + encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI);
-  authUrl += '&scope=' + encodeURIComponent(scopes);
+  let authUrl: string = `https://accounts.spotify.com/authorize`
+    + `?response_type=code`
+    + `&client_id=${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}`
+    + `&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI)}`
+    + `&scope=${encodeURIComponent(scopes)}`;
   window.location.assign(authUrl);
 }
 
