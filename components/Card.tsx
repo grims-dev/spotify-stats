8970263c18f5977802ac26d51eccaf01f52c5460
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-19 16:39:11 +0100

    Update CardProps types and fix some styling

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+type CardProps = {
+  index?: number;
+  imageURL: string;
+  text: string;
+}
+
 export default function Card({
   index,
   imageURL,
   text,
-}) {
+}: CardProps) {
   return (
     <div
       className="flex items-end overflow-hidden rounded-lg bg-cover bg-center h-44 relative text-white cursor-default"
@@ -15,9 +21,8 @@ export default function Card({
         style={{background: 'linear-gradient(0deg, #111111aa 25%, #ffffff00 80%'}}
       ></div>
       <div className="p-2 z-10">
-        <strong className="absolute top-0 right-0 p-1.5 bg-green-600 bg-opacity-95 w-10 text-center tracking-wide rounded-bl-sm shadow">#{index+1}</strong>
-        <strong className="text-lg leading-none" style={{textShadow: '0px 0px 5px #0000000099'}}>{text}</strong>
-        <br/>
+        {!isNaN(index) && <strong className="absolute top-0 right-0 p-1.5 bg-green-600 bg-opacity-95 w-10 text-center tracking-wide rounded-bl-sm shadow">#{index+1}</strong>}
+        <strong className="max-w-full text-lg leading-none break-words" style={{textShadow: '0px 0px 5px #0000000099'}}>{text}</strong>
       </div>
     </div>
   )
