0a26c60e83e975933567974b56a62a3007ca90e5
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-19 16:40:12 +0100

    Remove pointless Tailwind classes on component

--- components/TopArtistsOrTracksList.tsx
+++ components/TopArtistsOrTracksList.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import useTopArtistsOrTracks from '../hooks/useTopArtistsOrTracks';
 import Card from './Card';
 
-export default function TopArtistsOrTracksList({ accessToken , endpointOptions }) {
+export default function TopArtistsOrTracksList({ accessToken, endpointOptions }) {
   const { data, isLoading, isError } = useTopArtistsOrTracks(accessToken, endpointOptions);
   if (isLoading) return <>Loading...</>;
   if (isError) return <>An error occurred when connecting to Spotify. Please try reloading the page.</>;
 
   if (data?.items && data?.items?.length > 0) {
     return (
-      <ol className="list-none flex flex-wrap grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-5 justify-center text-base">
+      <ol className="list-none grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-5 justify-center text-base">
         {data.items.map((item, i) => {
           return (
             <li key={item.id}>
