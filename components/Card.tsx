9d0d53aec28922d98abb78294c66c0fac9a2f5fd
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-19 17:15:23 +0100

    Self-closing tag

--- components/Card.tsx
+++ components/Card.tsx
@@ -17,7 +17,7 @@ export default function Card({
       <div
         className="w-full h-full absolute"
         style={{background: 'linear-gradient(0deg, #111111aa 25%, #ffffff00 80%'}}
-      ></div>
+      />
       <div className="p-2 z-10">
         {!isNaN(index) && <strong className="absolute top-0 right-0 p-1.5 bg-green-600 bg-opacity-95 w-10 text-center tracking-wide rounded-bl-sm shadow">#{index+1}</strong>}
         <strong className="max-w-full text-lg leading-none break-words" style={{textShadow: '0px 0px 5px #0000000099'}}>{text}</strong>
