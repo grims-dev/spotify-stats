cf8486ccf3d863c9ab707c24ac05d4690ffd0812
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-20 17:02:25 +0100

    Tweak footer style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="pt-24 pb-4 text-gray-300 text-center row-start-2 row-end-3">
+    <div className="pt-20 pb-2 text-gray-300 text-sm text-center row-start-2 row-end-3">
       &copy; {(new Date()).getFullYear()} <a href="https://grims.dev/" target="_blank">Ciaran Grimshaw</a>
     </div>
   )
