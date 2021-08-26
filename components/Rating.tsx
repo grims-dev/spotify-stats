22f9a6384f6c4cabfb2bec4b3e3b610a7749fe8c
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-26 13:05:44 +0100

    Adding high, low, and optimum value to Rating component

--- components/Rating.tsx
+++ components/Rating.tsx
@@ -12,7 +12,15 @@ export default function Rating({
   return (
     <div className="text-center">
       {`${Math.round((value / maxValue) * 100)}%`}<br/>
-      <meter className="w-4/5" value={value} max={maxValue} min={minValue} />
+      <meter
+        className="w-4/5"
+        value={value}
+        max={maxValue}
+        min={minValue}
+        high={maxValue * 0.66}
+        low={maxValue * 0.33}
+        optimum={maxValue}
+      />
     </div>
   )
 }
