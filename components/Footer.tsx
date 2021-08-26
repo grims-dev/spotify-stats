8eccac317088aa4dca6ffedebdbbe99c7423b5b7
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-26 14:02:38 +0100

    Update footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="pt-20 pb-2 text-gray-300 text-sm text-center row-start-2 row-end-3">
-      &copy; {(new Date()).getFullYear()} <a href="https://grims.dev/" target="_blank">Ciaran Grimshaw</a>
+      &copy; {(new Date()).getFullYear()} <a href="https://grims.dev/" target="_blank">Ciaran Grimshaw</a> | All data provided by the <a href="https://developer.spotify.com/" target="_blank">Spotify API</a>
     </div>
   )
 }
