09de4db0ce028dac1114f0b9f75378b463e9c4b4
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-28 20:22:45 +0100

    Create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import Container from './Container';
+
+export default function Header() {
+  return (
+    <div className="sticky top-0 z-50 px-4 bg-gray-800 bg-opacity-95 border-b border-gray-900 border-opacity-50 shadow-md">
+      <Container>
+        <div className="h-14 flex items-center gap-3 text-center">
+          <span className="text-left flex-grow"><Link href="/">Home</Link></span>
+          <Link href="/top">Top Tracks</Link>
+          <span className="hidden sm:inline-block">•</span>
+          <Link href="/playlists">Playlist Stats</Link>
+        </div>
+      </Container>
+    </div>
+  )
+}
