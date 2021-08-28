b8f585da09fd00b9a3097aec9bc049f2976a87f3
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-28 20:20:47 +0100

    Amend margin on playlist description

--- components/PlaylistInfo.tsx
+++ components/PlaylistInfo.tsx
@@ -15,7 +15,7 @@ export default function PlaylistInfo({ accessToken, endpointOptions }) {
         />
         <div className="inline-block">
           <h1 className="text-5xl">{data.name}</h1>
-          {data.description && <p className="italic">{data.description}</p>}
+          {data.description && <p className="mt-2 italic">{data.description}</p>}
           <p className="mt-4">{data.public ? 'Public' : 'Private'} playlist by {data.owner.display_name}&nbsp;&nbsp;•&nbsp;&nbsp;{data.tracks.total} songs</p>
         </div>
       </div>
