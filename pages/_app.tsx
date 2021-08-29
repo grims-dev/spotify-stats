47229225f219bbce4ef6e3cfa040a6a7061dccd2
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-29 13:26:02 +0100

    Add additional router check for isHomepage logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import Container from '../components/Container';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -7,7 +8,8 @@ import PageLayout from '../components/PageLayout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const isHomepage = Component.name === 'Home';
+  const router = useRouter();
+  const isHomepage = Component.name === 'Home' || router.pathname === '/';
   return (
     <PageLayout isHomepage={isHomepage}>
       {!isHomepage && <Header />}
