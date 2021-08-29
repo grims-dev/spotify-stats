eca20ba13897b2edf3689731d1d322418397f335
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-29 18:55:40 +0100

    Improve fallbacks for missing bits of data

--- components/PlaylistInfo.tsx
+++ components/PlaylistInfo.tsx
@@ -7,13 +7,16 @@ export default function PlaylistInfo({ accessToken, endpointOptions }) {
   if (isError) return <>An error occurred when connecting to Spotify. Please try reloading the page.</>;
 
   if (Object.keys(data).length > 0) {
+    const imageURL = data?.images?.[0]?.url;
     return (
       <div className="text-base sm:flex sm:items-center">
-        <img
-          src={data?.images?.[0]?.url}
-          alt={`${data.name} playlist icon`}
-          className="inline-block mb-4 sm:mb-0 sm:mr-4 sm:w-1/3 md:w-1/4"
-        />
+        {imageURL && (
+          <img
+            src={imageURL}
+            alt={`${data.name} playlist icon`}
+            className="inline-block mb-4 sm:mb-0 sm:mr-4 sm:w-1/3 md:w-1/4"
+          />
+        )}
         <div className="inline-block">
           <h1 className="text-3xl sm:text-4xl md:text-5xl">{data.name}</h1>
           {data.description && <p className="mt-2 italic">{data.description}</p>}
--- components/PlaylistTracks.tsx
+++ components/PlaylistTracks.tsx
@@ -8,10 +8,13 @@ export default function MultipleTracksInfo({ accessToken, endpointOptions }) {
   if (isError) return <>An error occurred when connecting to Spotify. Please try reloading the page.</>;
 
   if (data) {
-    const trackIDs = data.items.map(item => item.track.id)
-    return (
-      <TracksAnalysis accessToken={accessToken} endpointOptions={{ trackIDs }} />
-    )
+    const trackIDs = data.items.map(item => item.track.id);
+    console.log(trackIDs);
+    if (trackIDs.length) {
+      return <TracksAnalysis accessToken={accessToken} endpointOptions={{ trackIDs }} />;
+    } else {
+      return <>This playlist has no tracks!</>;
+    }
   }
 
   return <>Your authentication has expired. Please try reloading the page.</>;
