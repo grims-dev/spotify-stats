81c113820a9d67c3f285d541c9cc524c72809222
grims-dev <ciarangrimshawdev@gmail.com>
2021-08-29 11:15:55 +0100

    Add additional meta tags

--- components/HeadTags.tsx
+++ components/HeadTags.tsx
@@ -7,6 +7,8 @@ export default function HeadTags({ title = '', customStyle = '' }) {
       <meta charSet="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <title>{titleText}</title>
+      <meta name="application-name" content="Spotify Stats" />
+      <meta name="theme-color" content="#1f2937" />
       <meta name="description" content="Log in with Spotify to view your favourite tracks and artists, recent listening habits, and playlist data."></meta>
       <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
       <link rel="preload" href="/fonts/Oxygen/Oxygen-Light.ttf" as="font" crossOrigin="" />
